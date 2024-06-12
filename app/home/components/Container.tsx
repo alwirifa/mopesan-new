afa573a527fae70311ed3cbec3e5a1cf1f1f83b4
Alwi <alwirifa7@gmail.com>
2024-06-12 16:47:21 +0700

    perbaikan collapse landing page

--- app/home/components/Container.tsx
+++ app/home/components/Container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="max-w-7xl mx-auto w-full">
+    <div className="max-w-7xl mx-auto w-full p-0 sm:p-6 md:p-8 xl:p-0">
       {children}
     </div>
   );
--- app/home/components/section4/Section4.tsx
+++ app/home/components/section4/Section4.tsx
@@ -4,25 +4,25 @@ import Image from "next/image";
 
 const Section4 = () => {
   return (
-    <div className="py-16 md:py-32 w-full z-10"> {/* Changed z-40 to z-10 */}
+    <div className="py-16 xl:py-32 w-full z-10"> {/* Changed z-40 to z-10 */}
       <Container>
         <div className="relative flex flex-col justify-center gap-4">
           <div className="px-6 flex justify-center">
             <img
               src="/home/heroImage.webp"
               alt=""
-              className="h-[30vh] md:h-[50vh] w-auto bg-contain md:absolute md:-top-8 md:left-20"
+              className="h-[30vh] xl:h-[50vh] w-auto bg-contain xl:absolute xl:-top-8 xl:left-20"
             />
           </div>
           <img
             src="/home/section4.svg"
-            className="h-[40vh] w-full bg-contain sm:flex hidden"
+            className="h-[40vh] w-full bg-contain xl:flex hidden"
           />
-          <div className="md:absolute md:top-6 md:right-16 max-w-xl p-4 md:p-16 flex flex-col gap-4">
-            <h1 className="text-2xl font-semibold md:text-white text-primary leading-snug">
+          <div className="xl:absolute xl:top-6 xl:right-16 max-w-xl p-4 xl:p-16 flex flex-col gap-4">
+            <h1 className="text-2xl font-semibold xl:text-white text-primary leading-snug">
               Unduh Sekarang dan nikmati promo menarik tiap harinya.
             </h1>
-            <p className="text-primary md:text-white">
+            <p className="text-primary xl:text-white">
               Gak perlu lagi mikirin lapar karena pesan makanan jadi super
               gampang! Temukan beragam makanan kekinian favoritmu. Mulai dari
               jajanan hits sampai makanan favoritmu,
